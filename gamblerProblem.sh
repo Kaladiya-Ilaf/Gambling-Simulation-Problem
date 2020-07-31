3118a24e65f4e2465abe8c4ba8c5167b8aa663df
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-07-31 11:45:45 +0530

    Added: Condition to gamable until 50% stake won or lose.

--- gamblerProblem.sh
+++ gamblerProblem.sh
@@ -4,16 +4,24 @@
 STAKE=100
 BET=1
 
-currentStake=STAKE
-currentBet=$(( RANDOM % 2 ))
+#initializinf variables
+currentStake=$STAKE
+percent=$(awk "BEGIN { p=100*50/${STAKE}; i=int(p); print (p-i<0.5)?i:i+1 }")
+maxLimit=`expr $STAKE + $percent`
+minLimit=`expr $STAKE - $percent`
 
-if [ $currentBet -eq 1 ]
-then
-	echo "You have won this round!"
-	currentStake=$(( $currentStake + $BET ))
-else
-	echo "You have lost this round!"
-	currentStake=$(( $currentStake - $BET ))
-fi
-
-echo "Current Balance :" $currentStake
+#condition to continue gambling until 50% of stake is won or lose
+while [ $currentStake -gt $minLimit ] && [ $currentStake -lt $maxLimit ]
+do
+   currentBet=$((RANDOM % 2))
+	if [ $currentBet -eq 1 ]
+	then
+		echo "You have won this round!"
+		currentStake=$(( $currentStake + $BET ))
+		echo "Current Balance :" $currentStake
+	else
+		echo "You have lost this round!"
+		currentStake=$(( $currentStake - $BET ))
+		echo "Current Balance :" $currentStake
+	fi
+done
