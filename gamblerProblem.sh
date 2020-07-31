a009ed94f9d3c2668ae697b20232d49b684df28a
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-07-31 11:36:42 +0530

    Added: Condition to win or fail.

--- gamblerProblem.sh
+++ gamblerProblem.sh
@@ -3,3 +3,17 @@
 #Intializing the constanats
 STAKE=100
 BET=1
+
+currentStake=STAKE
+currentBet=$(( RANDOM % 2 ))
+
+if [ $currentBet -eq 1 ]
+then
+	echo "You have won this round!"
+	currentStake=$(( $currentStake + $BET ))
+else
+	echo "You have lost this round!"
+	currentStake=$(( $currentStake - $BET ))
+fi
+
+echo "Current Balance :" $currentStake
